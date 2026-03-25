120ee76697dc0cc148c91879f9997a7c8bcc3571
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-25 08:46:41 -0300

    first commit - ms-pedidos

---
+++ ms-pedidos/src/test/java/com/github/yasminmuniz25/ms_pedidos/MsPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yasminmuniz25.ms_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
