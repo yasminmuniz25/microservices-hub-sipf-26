f8f2e3beadff2e35bf88aa521906e0358de523f8
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-01 11:20:10 -0300

    atualizações

---
+++ ms-pagamentos/src/main/java/com/github/yasminmuniz25/ms/pagamentos/MsPagamentosApplication.java
@@ -0,0 +1,13 @@
+package com.github.yasminmuniz25.ms.pagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPagamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPagamentosApplication.class, args);
+	}
+
+}
