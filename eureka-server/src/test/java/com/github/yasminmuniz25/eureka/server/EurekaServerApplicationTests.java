b5d7317f96335a7e1453d59b8ee04ee3adb2d21c
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-06 11:38:27 -0300

    add Eureka-server, config MS-Pedidos client eureka

---
+++ eureka-server/src/test/java/com/github/yasminmuniz25/eureka/server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yasminmuniz25.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- ms-pedidos/src/main/java/com/github/yasminmuniz25/ms_pedidos/MsPedidosApplication.java
+++ ms-pedidos/src/main/java/com/github/yasminmuniz25/ms_pedidos/MsPedidosApplication.java
@@ -2,9 +2,11 @@ package com.github.yasminmuniz25.ms_pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class  MsPedidosApplication {
+@EnableDiscoveryClient
+public class MsPedidosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsPedidosApplication.class, args);
